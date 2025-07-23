98d735c48f17b683b0a5bb712a7a804a3764849f
Reischz <rutjanaret.u@gmail.com>
2025-07-23 16:37:24 +0700

    set max y

--- root_analysisCode/plot_allFinalProcessInfo.cpp
+++ root_analysisCode/plot_allFinalProcessInfo.cpp
@@ -24,6 +24,10 @@ void plot_allFinalProcessInfo()
 	
 	TCanvas *pT_canvas = new TCanvas();
 	pT_canvas->cd();
+	double maxY = std::max({Jet_PT_hist->GetMaximum(), Electron_Pt_hist->GetMaximum(), Muon_Pt_hist->GetMaximum()});
+	Jet_PT_hist->SetMaximum(maxY * 1.2); 
+
+	
 	Jet_PT_hist->Draw();
 	Jet_PT_hist->SetStats(0);  // Disable default stats box
 	//Jet_PT_hist->SetFillColor(kBlue);
