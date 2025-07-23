df33c6d714ee84c97a94ef0c320595736a2acf91
Reischz <rutjanaret.u@gmail.com>
2025-07-23 16:15:16 +0700

    invmassV1

--- root_analysisCode/try_invariantMass.cpp
+++ root_analysisCode/try_invariantMass.cpp
@@ -14,17 +14,24 @@ void plot_allFinalProcessInfo()
 	{
 		delphes_tree->GetEntry(jentry);
 		// check conditions to ensure InvariantMass is valid
-		if (delphes_tree->Electron_size == 2)
+		double invmass_value = 0;
+		if (delphes_tree->Electron_size == 2 && delphes_tree->Electron_Charge[0] * delphes_tree->Electron_Charge[1] == -1) 
 		{
-			
-		}
-		invmass->Fill(delphes_tree->InvariantMass);
+			invmass_value = sqrt(2*delphes_tree->Electron_PT[0]*delphes_tree->Electron_PT[1]*(cosh(
+				delphes_tree->Electron_Eta[0] - delphes_tree->Electron_Eta[1]) - cos(delphes_tree->Electron_Phi[0] - 
+				delphes_tree->Electron_Phi[1])));
+		} else if (delphes_tree->Muon_size == 2 && delphes_tree->Muon_Charge[0] * delphes_tree->Muon_Charge[1] == -1) 
+		{
+			invmass_value = sqrt(2*delphes_tree->Muon_PT[0]*delphes_tree->Muon_PT[1]*(cosh(
+				delphes_tree->Muon_Eta[0] - delphes_tree->Muon_Eta[1]) - cos(delphes_tree->Muon_Phi[0] - 
+				delphes_tree->Muon_Phi[1])));
 
+		}
+		invmass->Fill(invmass_value);
 	}
 	
 	TCanvas *invmass_canvas = new TCanvas();
 	invmass_canvas->cd();
 	invmass->Draw();
 	invmass_canvas->SaveAs("invmass.png");
-
 }
