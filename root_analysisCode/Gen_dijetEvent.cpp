cba9e908570e43b72f68f5b74edc8ad6dd3ec77e
Reischz <rutjanaret.u@gmail.com>
2025-07-24 14:46:09 +0700

    update

--- root_analysisCode/Gen_dijetEvent.cpp
+++ root_analysisCode/Gen_dijetEvent.cpp
@@ -118,7 +118,5 @@ void Gen_dijetEvent()
 	hist2d->Draw("COLZ");
 	hist2d_canvas->SaveAs("GenJethist2d.png");
 	
-
-	std::cout << M_PI << std::endl; // Print the value of M_PI to verify it's defined correctly
 	gApplication->Terminate(0); // Exit ROOT with code 0
 }
