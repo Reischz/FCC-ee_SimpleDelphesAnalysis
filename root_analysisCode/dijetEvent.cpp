3718cc7b761c1d58825632625f32aab26f761a70
Reischz <rutjanaret.u@gmail.com>
2025-07-28 11:46:41 +0700

    modified dijet

--- root_analysisCode/dijetEvent.cpp
+++ root_analysisCode/dijetEvent.cpp
@@ -15,23 +15,29 @@ double deltaPhi(double phi1, double phi2) {
 
 void dijetEvent()
 {
+	gApplication->SetOptStat(0);  // Hide stats box
 	TFile *file = new TFile("output01.root");
 	TTree *tree = (TTree*) file->Get("Delphes");
 
 	Delphes *delphes_tree = new Delphes(tree);
+	THStack *InvMass = new THStack("InvMass", "Invariant Mass; GeV; Entries");
+	THStack *JetDist = new THStack("JetDist", "Jets Distribution; Number of Jets; Events");
 	TH2D *hist2d = new TH2D("hist2d", "two-dimensional histogram;#Delta #Phi;Inv. Mass", 60, 0, 6, 210, 80, 300);
 
-	TH1F *invmass = new TH1F("invmass", "Invariant Mass", 70, 50, 120);
+	TH1F *Einvmass = new TH1F("Einvmass", "Electron Pair", 70, 50, 120);
+	TH1F *Minvmass = new TH1F("Minvmass", "Muon Pair", 70, 50, 120);
 
 	// try using btag to find higgs invariant mass
 	TH1F *higgsInvM = new TH1F("higgsInvM", "Higgs Invariant Mass", 90, 50, 240);
 	TH1F *higgsInvM2Jet = new TH1F("higgsInvM2Jet", "Higgs Invariant Mass w/ 2 Jets", 75, 90, 240); // IGNORE
+	TH1F *BtaggedJets = new TH1F("BtaggedJets", "B-Tagged Jets", 5, -0.5, 4.5);
+	TH1F *Btagged2Jets = new TH1F("Btagged2Jets", "B-Tagged Dijet", 5, -0.5, 4.5);
+	TH1F *JetSize = new TH1F("JetSize", "All Jets", 5, -0.5, 4.5);
 
 	for (Long64_t jentry=0; jentry<tree->GetEntries(); jentry++)
 	{
 		delphes_tree->GetEntry(jentry);
 		// check conditions to ensure InvariantMass is valid
-		double invmass_value = 0;
 		double higgsInvM_value = 0;
 		double higgsInvM2Jet_value = 0;
 		double DeltaR_value = 0;
@@ -39,15 +45,17 @@ void dijetEvent()
 		 
 		// loop to find all Btagged jets
 		int btaggedJets = 0;
-		for (int i = 0; i < delphes_tree->Jet_size; i++) 
+		int JetSize_value = delphes_tree->Jet_size;
+		JetSize->Fill(JetSize_value);
+		for (int i = 0; i < JetSize_value; i++) 
 		{
 			btaggedJets += delphes_tree->Jet_BTag[i];
 		}
-
+		BtaggedJets->Fill(btaggedJets);
 		if (btaggedJets == 2) 
 		{
 			std::vector<int> btaggedJetIndex;
-			for (int i = 0; i < delphes_tree->Jet_size; i++) 
+			for (int i = 0; i < JetSize_value; i++) 
 			{
 				if (delphes_tree->Jet_BTag[i]==1) 
 				{
@@ -58,8 +66,9 @@ void dijetEvent()
 			higgsInvM_value = sqrt(2 * delphes_tree->Jet_PT[btaggedJetIndex[0]] * delphes_tree->Jet_PT[btaggedJetIndex[1]] *
 				(cosh(delphes_tree->Jet_Eta[btaggedJetIndex[0]] - delphes_tree->Jet_Eta[btaggedJetIndex[1]]) - 
 				cos(deltaPhi(delphes_tree->Jet_Phi[btaggedJetIndex[0]], delphes_tree->Jet_Phi[btaggedJetIndex[1]]))));
-			if (delphes_tree->Jet_size == 2) 
+			if (JetSize_value == 2) 
 			{
+				Btagged2Jets->Fill(btaggedJets);
 				DeltaPhi_value = deltaPhi(delphes_tree->Jet_Phi[0], delphes_tree->Jet_Phi[1]);
 				DeltaR_value = sqrt(pow(delphes_tree->Jet_Eta[0] - delphes_tree->Jet_Eta[1], 2) + 
 				pow(deltaPhi(delphes_tree->Jet_Phi[0], delphes_tree->Jet_Phi[1]), 2));
@@ -73,23 +82,20 @@ void dijetEvent()
 
 		if (delphes_tree->Electron_size == 2 && delphes_tree->Electron_Charge[0] * delphes_tree->Electron_Charge[1] == -1) 
 		{
-			invmass_value = sqrt(2*delphes_tree->Electron_PT[0]*delphes_tree->Electron_PT[1]*(cosh(
+			double invmass_value = sqrt(2*delphes_tree->Electron_PT[0]*delphes_tree->Electron_PT[1]*(cosh(
 						delphes_tree->Electron_Eta[0] - delphes_tree->Electron_Eta[1]) - cos(deltaPhi(delphes_tree->Electron_Phi[0], 
 						delphes_tree->Electron_Phi[1]))));
+			Einvmass->Fill(invmass_value);
 		} else if (delphes_tree->Muon_size == 2 && delphes_tree->Muon_Charge[0] * delphes_tree->Muon_Charge[1] == -1) 
 		{
-			invmass_value = sqrt(2*delphes_tree->Muon_PT[0]*delphes_tree->Muon_PT[1]*(cosh(
+			double invmass_value = sqrt(2*delphes_tree->Muon_PT[0]*delphes_tree->Muon_PT[1]*(cosh(
 				delphes_tree->Muon_Eta[0] - delphes_tree->Muon_Eta[1]) - cos(deltaPhi(delphes_tree->Muon_Phi[0], 
 				delphes_tree->Muon_Phi[1]))));
-
+			Minvmass->Fill(invmass_value);
 		}
 		if (higgsInvM_value > 0) 
 		{
 			higgsInvM->Fill(higgsInvM_value);
-		}
-		if (invmass_value > 0) 
-		{
-			invmass->Fill(invmass_value);
 		}
 		if (higgsInvM2Jet_value > 0) 
 		{
@@ -98,27 +104,27 @@ void dijetEvent()
 		}
 	}
 	
-	TCanvas *higgsInvM_canvas = new TCanvas();
-	higgsInvM_canvas->cd();
-	higgsInvM->Draw();
-	higgsInvM_canvas->SaveAs("higgsInvM.png");
-
-	TCanvas *invmass_canvas = new TCanvas();
-	invmass_canvas->cd();
-	invmass->Draw();
-	invmass_canvas->SaveAs("invmass.png");
-
-	TCanvas *higgsInvM2Jet_canvas = new TCanvas();
-	higgsInvM2Jet_canvas->cd();
+	TCanvas *c1 = new TCanvas();
+	c1->cd();
+	InvMass->Add(higgsInvM);
+	InvMass->Add(Einvmass);
+	InvMass->Add(Minvmass);
+	InvMass->Draw();
+	c1->BuildLegend(0.65, 0.75, 0.88, 0.88);
+	c1->SaveAs("InvariantMass.png");
+	c1->Clear();
+	JetDist->Add(JetSize);
+	JetDist->Add(BtaggedJets);
+	JetDist->Add(Btagged2Jets);
+	JetDist->Draw();
+	c1->BuildLegend(0.65, 0.75, 0.88, 0.88);
+	c1->SaveAs("JetDistribution.png");
+	c1->Clear();
+	higgsInvM2Jet->SetTitle("Higgs Invariant Mass with 2 Jets; GeV; Entries");
 	higgsInvM2Jet->Draw();
-	higgsInvM2Jet_canvas->SaveAs("higgsInvM2Jet.png");
+	c1->BuildLegend(0.65, 0.75, 0.88, 0.88);
+	c1->SaveAs("HiggsInvM2Jet.png");
 
-	TCanvas *hist2d_canvas = new TCanvas();
-	hist2d_canvas->cd();
-	hist2d->Draw("COLZ");
-	hist2d_canvas->SaveAs("hist2d.png");
 	
-
-	std::cout << M_PI << std::endl; // Print the value of M_PI to verify it's defined correctly
 	gApplication->Terminate(0); // Exit ROOT with code 0
 }
