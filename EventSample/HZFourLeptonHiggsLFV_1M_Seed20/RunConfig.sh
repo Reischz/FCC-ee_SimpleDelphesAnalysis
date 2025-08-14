2d1d1c396ad9be26f91af96a81fd06fad99ce31e
Reischz <rutjanaret.u@gmail.com>
2025-08-14 11:32:31 +0700

    Increase the number of events in mg5Card.dat and Pythia configuration to 1,000,000 for enhanced simulation accuracy

--- EventSample/HZFourLeptonHiggsLFV_1M_Seed20/RunConfig.sh
+++ EventSample/HZFourLeptonHiggsLFV_1M_Seed20/RunConfig.sh
@@ -15,7 +15,7 @@ mv eeToZH_HLFV_FourLeptons/Events/formal01/unweighted_events.lhe.gz .
 # pythia8 events
 echo 'Running Pythia8'
 cp ../../PythiaCard_HiggsEMuLFV.cmd .
-echo -e '\nMain:numberOfEvents      = 1000' >> PythiaCard_HiggsEMuLFV.cmd
+echo -e '\nMain:numberOfEvents      = 1000000' >> PythiaCard_HiggsEMuLFV.cmd
 echo 'Random:setSeed = on                ! Turn on the seed setting' >> PythiaCard_HiggsEMuLFV.cmd
 echo 'Random:seed = 20' >> PythiaCard_HiggsEMuLFV.cmd
 echo 'Beams:LHEF=unweighted_events.lhe.gz' >> PythiaCard_HiggsEMuLFV.cmd
