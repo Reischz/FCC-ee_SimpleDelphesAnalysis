098885463098c31cef6eaa89137da0820566592e
Reischz <rutjanaret.u@gmail.com>
2025-08-17 17:54:52 +0700

    Fix subprocess call in pythontest.py to use capture_output for better output handling

--- pythontest.py
+++ pythontest.py
@@ -6,6 +6,4 @@ from datetime import datetime
 import subprocess
 
 # os.system("/work/home/ruttho/binary/MG5_aMC_v2_9_24/bin/mg5_aMC testmadgraph.dat")
-subprocess.run(["/work/home/ruttho/binary/MG5_aMC_v2_9_24/bin/mg5_aMC", "testmadgraph.dat"], check=True, capture=True, text=True)
-#;eojepwojwpfojwf
-#o;vjpovj
+subprocess.run(["/work/home/ruttho/binary/MG5_aMC_v2_9_24/bin/mg5_aMC", "testmadgraph.dat"], capture_output=True, text=True, check=True)
