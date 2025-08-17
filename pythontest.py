c59015e426a411183d36cd1309999c558451a6fc
Reischz <rutjanaret.u@gmail.com>
2025-08-17 17:46:51 +0700

    Add a comment placeholder in pythontest.py for future reference

--- pythontest.py
+++ pythontest.py
@@ -7,3 +7,4 @@ import subprocess
 
 # os.system("/work/home/ruttho/binary/MG5_aMC_v2_9_24/bin/mg5_aMC testmadgraph.dat")
 subprocess.run(["/work/home/ruttho/binary/MG5_aMC_v2_9_24/bin/mg5_aMC", "testmadgraph.dat"], check=True, capture=True, text=True)
+#;eojepwojwpfojwf
