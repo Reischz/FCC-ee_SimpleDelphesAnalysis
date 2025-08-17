c3dca4729a7f06b1ed1063926f220b701bbe120f
Reischz <rutjanaret.u@gmail.com>
2025-08-17 19:16:02 +0700

    Refactor subprocess call in pythontest.py to capture output and log results

--- pythontest.py
+++ pythontest.py
@@ -6,4 +6,6 @@ from datetime import datetime
 import subprocess
 
 # os.system("/work/home/ruttho/binary/MG5_aMC_v2_9_24/bin/mg5_aMC testmadgraph.dat")
-subprocess.run(["/work/home/ruttho/binary/MG5_aMC_v2_9_24/bin/mg5_aMC", "testmadgraph.dat"], capture_output=True, text=True, check=True)
+testoscomand=subprocess.run(["/work/home/ruttho/binary/MG5_aMC_v2_9_24/bin/mg5_aMC", "testmadgraph.dat"], capture_output=True, text=True, check=True)
+with open('test_output.log', 'w') as f:
+    f.write(testoscomand.stdout + '\n' + testoscomand.stderr)
