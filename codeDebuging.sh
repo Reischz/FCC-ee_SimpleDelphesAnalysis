7cda85bbf10d32123124d99e1f2c1063d65d4c71
Reischz <rutjanaret.u@gmail.com>
2025-08-17 16:51:09 +0700

    Add initial versions of codeDebuging.sh, pythontest.py, and testmadgraph.dat

---
+++ codeDebuging.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#SBATCH --job-name=test
+#SBATCH --qos=cu_hpc
+#SBATCH --partition=cpu
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=10
+#SBATCH --mem=8G
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+__conda_setup="$('/work/home/ruttho/binary/miniconda3/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
+if [ $? -eq 0 ]; then
+    eval "$__conda_setup"
+else
+    if [ -f "/work/home/ruttho/binary/miniconda3/etc/profile.d/conda.sh" ]; then
+        . "/work/home/ruttho/binary/miniconda3/etc/profile.d/conda.sh"
+    else
+        export PATH="/work/home/ruttho/binary/miniconda3/bin:$PATH"
+    fi
+fi
+unset __conda_setup
+# <<< conda initialize <<<
+
+source /work/app/share_env/hepsw-gcc11p2-py3p9p9.sh
+conda activate ext
+/work/home/ruttho/binary/miniconda3/envs/ext/bin/python orchestratedFile.py
---
+++ pythontest.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+from lib.MadGraphPythiaDelphes_Chain import GenerateSignalChains
+import os
+import yaml
+from datetime import datetime
+
+os.system("/work/home/ruttho/binary/MG5_aMC_v2_9_24/bin/mg5_aMC testmadgraph.dat")
