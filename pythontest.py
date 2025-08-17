185252ebf37d3c8163777226af32264d76fd7595
Reischz <rutjanaret.u@gmail.com>
2025-08-17 17:34:41 +0700

    Refactor pythontest.py to use subprocess for executing MadGraph command

--- pythontest.py
+++ pythontest.py
@@ -3,5 +3,7 @@ from lib.MadGraphPythiaDelphes_Chain import GenerateSignalChains
 import os
 import yaml
 from datetime import datetime
+import subprocess
 
-os.system("/work/home/ruttho/binary/MG5_aMC_v2_9_24/bin/mg5_aMC testmadgraph.dat")
+# os.system("/work/home/ruttho/binary/MG5_aMC_v2_9_24/bin/mg5_aMC testmadgraph.dat")
+subprocess.run(["/work/home/ruttho/binary/MG5_aMC_v2_9_24/bin/mg5_aMC", "testmadgraph.dat"])
