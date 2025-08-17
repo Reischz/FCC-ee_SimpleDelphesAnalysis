5b6af1489670635865612ececa5ba12ddeda2074
Reischz <rutjanaret.u@gmail.com>
2025-08-17 17:44:13 +0700

    Enhance subprocess call in pythontest.py to include error checking and output capture

--- pythontest.py
+++ pythontest.py
@@ -6,4 +6,4 @@ from datetime import datetime
 import subprocess
 
 # os.system("/work/home/ruttho/binary/MG5_aMC_v2_9_24/bin/mg5_aMC testmadgraph.dat")
-subprocess.run(["/work/home/ruttho/binary/MG5_aMC_v2_9_24/bin/mg5_aMC", "testmadgraph.dat"])
+subprocess.run(["/work/home/ruttho/binary/MG5_aMC_v2_9_24/bin/mg5_aMC", "testmadgraph.dat"], check=True, capture=True, text=True)
