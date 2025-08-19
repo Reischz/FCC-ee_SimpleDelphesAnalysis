6a290ad0bc05ca7cf3f9fb21aa0a2cd0432c2bd2
Reischz <rutjanaret.u@gmail.com>
2025-08-19 12:39:54 +0700

    Refactor codeDebuging.sh to comment out conda initialization and update testmadgraph.dat to set number of cores

--- codeDebuging.sh
+++ codeDebuging.sh
@@ -11,29 +11,30 @@
 source /work/app/share_env/hepsw-gcc11p2-py3p9p9.sh
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
-__conda_setup="$('/work/home/ruttho/binary/miniconda3/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
-if [ $? -eq 0 ]; then
-    echo "DEBUG: Using conda hook method"
-    eval "$__conda_setup"
-else
-    if [ -f "/work/home/ruttho/binary/miniconda3/etc/profile.d/conda.sh" ]; then
-        echo "DEBUG: Using conda.sh profile script"
-        . "/work/home/ruttho/binary/miniconda3/etc/profile.d/conda.sh"
-    else
-        echo "DEBUG: Using PATH fallback method"
-        export PATH="/work/home/ruttho/binary/miniconda3/bin:$PATH"
-    fi
-fi
-unset __conda_setup
-# <<< conda initialize <<<
+# __conda_setup="$('/work/home/ruttho/binary/miniconda3/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
+# if [ $? -eq 0 ]; then
+#     echo "DEBUG: Using conda hook method"
+#     eval "$__conda_setup"
+# else
+#     if [ -f "/work/home/ruttho/binary/miniconda3/etc/profile.d/conda.sh" ]; then
+#         echo "DEBUG: Using conda.sh profile script"
+#         . "/work/home/ruttho/binary/miniconda3/etc/profile.d/conda.sh"
+#     else
+#         echo "DEBUG: Using PATH fallback method"
+#         export PATH="/work/home/ruttho/binary/miniconda3/bin:$PATH"
+#     fi
+# fi
+# unset __conda_setup
+# # <<< conda initialize <<<
 
-# Force conda environment Python to take precedence by moving it to front of PATH
-export PATH="/work/home/ruttho/binary/miniconda3/envs/ext/bin:$PATH"  # NEEDED - conda activate doesn't work in HEP env!
+# # Force conda environment Python to take precedence by moving it to front of PATH
+# export PATH="/work/home/ruttho/binary/miniconda3/envs/ext/bin:$PATH"  # NEEDED - conda activate doesn't work in HEP env!
 
-# /work/home/ruttho/binary/miniconda3/envs/ext/bin/python pythontest.py
-echo "Tell me which python is being used"
-which python
-echo "Conda environment python:"
-/work/home/ruttho/binary/miniconda3/envs/ext/bin/python --version
-echo "Python path:"
-echo $PATH
+# # /work/home/ruttho/binary/miniconda3/envs/ext/bin/python pythontest.py
+# echo "Tell me which python is being used"
+# which python
+# echo "Conda environment python:"
+# /work/home/ruttho/binary/miniconda3/envs/ext/bin/python --version
+# echo "Python path:"
+# echo $PATH
+/work/home/ruttho/binary/MG5_aMC_v2_9_24/bin/mg5_aMC testmadgraph.dat
