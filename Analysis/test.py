3497d22234f267ceccf41cd79546e04b6b368ab8
Reischz <rutjanaret.u@gmail.com>
2025-08-29 09:06:29 +0700

    test

--- Analysis/test.py
+++ Analysis/test.py
@@ -12,5 +12,8 @@ HZ4Lep = uproot.open("Bg1_hz.root")
 HZ4Lep_tree = HZ4Lep["Delphes"]
 # Assuming HZ4LepLFV_tree is your tree object
 all_column_names = HZ4Lep_tree.keys()
-
 print(all_column_names)
+# see type of Electron/Electron.Eta
+HZ4Lep_df = HZ4Lep_tree.arrays(["Electron/Electron.Eta"], library="pd", entry_stop=5)
+print(HZ4Lep_df)
+print(type(HZ4Lep_df["Electron"]["Electron.Eta"][0]))
