d434b18bdbc706d610a1bbee521c302094a1e719
Reischz <rutjanaret.u@gmail.com>
2025-08-29 08:58:39 +0700

    Add analysis scripts for HZ4Lep data and include initial setup in scratch_test.ipynb

--- Analysis/test.py
+++ Analysis/test.py
@@ -10,6 +10,7 @@ start_time = time.perf_counter()
 print("Starting analysis...")
 HZ4Lep = uproot.open("Bg1_hz.root")
 HZ4Lep_tree = HZ4Lep["Delphes"]
-column_arrays = ["MissingET_size","MissingET_Eta","MissingET_Phi","Jet_size","Electron_size","Electron_Eta","Electron_Phi","Muon_size","Muon_Eta","Muon_Phi"]
-HZ4Lep_array = HZ4Lep_tree.arrays(column_arrays, library="pd", entry_stop=1000)
-print(type(HZ4Lep_array["Electron_Eta"][0]))
+# Assuming HZ4LepLFV_tree is your tree object
+all_column_names = HZ4Lep_tree.keys()
+
+print(all_column_names)
