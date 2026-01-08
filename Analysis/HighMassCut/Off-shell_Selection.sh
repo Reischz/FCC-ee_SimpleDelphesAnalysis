8a39c4aadc57e8503ad80a41dba6f8059c9bad07
Reischz <rutjanaret.u@gmail.com>
2026-01-08 13:09:40 +0700

    Fix script to call the correct off-shell cut file in selection process

--- Analysis/HighMassCut/Off-shell_Selection.sh
+++ Analysis/HighMassCut/Off-shell_Selection.sh
@@ -38,7 +38,7 @@ RESULT_NAME=(
 
 for i in "${!FILELIST[@]}"; do
     echo "Processing file ${FILELIST[$i]}"
-    root -l -b -q "../Normal_on_shell_cut.cpp(\"${FILELIST[$i]}\", \"${RESULT_NAME[$i]}_SelectionResults.root\", \"${RESULT_NAME[$i]}_AdditionalTree.root\")" &
+    root -l -b -q "../Z_off_shell_cut.cpp(\"${FILELIST[$i]}\", \"${RESULT_NAME[$i]}_SelectionResults.root\", \"${RESULT_NAME[$i]}_AdditionalTree.root\")" &
     # Limit the number of concurrent jobs to 6
     # if (( (i + 1) % 6 == 0 )); then
     #     wait
