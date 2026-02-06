dabd0e20e45db1991c5fec5adf5a2171190faf2b
Reischz <rutjanaret.u@gmail.com>
2026-02-06 15:12:33 +0700

    Fix type in GenMassIDentify function by changing map type from string to TString for lepton identification

--- Analysis/HighMassCut/lib/selectionlist.h
+++ Analysis/HighMassCut/lib/selectionlist.h
@@ -531,7 +531,7 @@ class Verify_Generator : public AnalysisModule {
 };
 
 void GenMassIDentify(EventContext &data, const defaultParameters &params){
-    map<string, vector<int>> GenLepFnLst = IdentifyImediateLepton(data);
+    map<TString, vector<int>> GenLepFnLst = IdentifyImediateLepton(data);
     vector<TLorentzVector> GenLepVecs;
     int LeptonSize = GenLepFnLst["Index"].size(), ThisLepPID;
     float ThisLepMass;
